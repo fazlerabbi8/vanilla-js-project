fb31ea346f951a5ed84a5018a1a85eba96d6b43d
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-05 15:56:18 +0600

    showing content when button is clicked.

--- Js/index.js
+++ Js/index.js
@@ -1,13 +1,28 @@
 // Donation + History button color change functionality
-    function setActive(name) {
-        const donationBtn = document.getElementById('btn-donation');
-        const historyBtn = document.getElementById('btn-history');
+function setActive(name) {
+  const donationBtn = document.getElementById("btn-donation");
+  const historyBtn = document.getElementById("btn-history");
 
-        if (name === 'donation') {
-            donationBtn.className = 'bg-[#B4F461] border-none text-black font-semibold btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl';
-            historyBtn.className = 'btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl';
-        } else {
-            historyBtn.className = 'bg-[#B4F461] border-none text-black font-semibold btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl';
-            donationBtn.className = 'bg-white border border-gray-300 text-black font-semibold btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl';
-        }
-    }
+  //   content changing
+  const donationSection = document.getElementById("donation-section");
+  const historySection = document.getElementById("history-section");
+
+  if (name === "donation") {
+    donationBtn.className =
+      "bg-[#B4F461] border-none text-black font-semibold btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl";
+    historyBtn.className = "btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl";
+
+    // showing and hiding content
+    donationSection.classList.remove("hidden");
+    historySection.classList.add("hidden");
+  } else {
+    historyBtn.className =
+      "bg-[#B4F461] border-none text-black font-semibold btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl";
+    donationBtn.className =
+      "bg-white border border-gray-300 text-black font-semibold btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl";
+
+    // showing and hiding content
+    historySection.classList.remove("hidden");
+    donationSection.classList.add("hidden");
+  }
+}
