14ba8a6c2fd789f9e1974a06b6acae8069fb9432
fazlerabbi13 <mohammad0132794@gmail.com>
2026-06-06 13:14:17 +0600

    create history for all donation(transaction) with local time.

--- Js/donation.js
+++ Js/donation.js
@@ -25,6 +25,15 @@ document.getElementById("noakhali").addEventListener("click", function (event) {
 
   document.getElementById("noakhali-balance").innerText = newBalance;
   document.getElementById("total-balance").innerText = reduceTotalBalance;
+
+  // all donation history for qouta
+  const createDiv = document.createElement("div");
+  createDiv.classList.add("p-4", "border", "rounded", "mt-3");
+  createDiv.innerHTML = `
+    <h3 class="text-2xl font-semibold">${donationMoney} BDT donated for Noakhali Flood</h3>
+    <p class="text-gray-500 text-sm">${new Date().toLocaleString()}</p>
+`;
+  document.getElementById("donation-container").appendChild(createDiv);
 });
 
 // Donation for Feni
@@ -55,6 +64,15 @@ document.getElementById("feni").addEventListener("click", function (event) {
 
   document.getElementById("feni-balance").innerText = newBalance;
   document.getElementById("total-balance").innerText = reduceTotalBalance;
+
+  // all donation history for qouta
+  const createDiv = document.createElement("div");
+  createDiv.classList.add("p-4", "border", "rounded", "mt-3");
+  createDiv.innerHTML = `
+    <h3 class="text-2xl font-semibold">${donationMoney} BDT donated for Feni Flood</h3>
+    <p class="text-gray-500 text-sm">${new Date().toLocaleString()}</p>
+`;
+  document.getElementById("donation-container").appendChild(createDiv);
 });
 
 // Donation for Qouta
@@ -85,4 +103,13 @@ document.getElementById("qouta").addEventListener("click", function (event) {
 
   document.getElementById("qouta-balance").innerText = newBalance;
   document.getElementById("total-balance").innerText = reduceTotalBalance;
+
+  // all donation history for qouta
+  const createDiv = document.createElement("div");
+  createDiv.classList.add("p-4", "border", "rounded", "mt-3");
+  createDiv.innerHTML = `
+    <h3 class="text-2xl font-semibold">${donationMoney} BDT donated for Qouta Relief</h3>
+    <p class="text-gray-500 text-sm">${new Date().toLocaleString()}</p>
+`;
+  document.getElementById("donation-container").appendChild(createDiv);
 });
